8750827c71a945020fdf6589ed5a18e6a58822d8
Yy <akumakumu@gmail.com>
2024-01-16 14:59:19 +0700

    basic example consumer

--- kafka-consumer.py
+++ kafka-consumer.py
@@ -1,24 +1,21 @@
-#from dotenv import load_dotenv
-#import os
-
 from kafka import KafkaConsumer
 
-#load_dotenv()
+#consumer = KafkaConsumer(bootstrap_servers='10.11.13.81:9092')
 
-#ip_addr = os.getenv("IP_ADDR")
-#port = os.getenv("PORT")
+#consumer.subscribe(['yytest'])
 
-from kafka import KafkaConsumer
+#FORMAT
+#consumer = KafkaConsumer(
+#    subs_topic,
+#    bootstrap_servers='ip-address:port',
+#    auto_offset_reset='earliest' # consume from beginning topic
+#)
 
-# Replace 'your_kafka_bootstrap_servers' with the actual address of your Kafka broker
-bootstrap_servers = '10.11.13.81'
+consumer = KafkaConsumer(
+    'yytest',
+    bootstrap_servers='10.11.13.81:9092',
+    auto_offset_reset='earliest'
+)
 
-# Create a KafkaConsumer instance
-consumer = KafkaConsumer('yy_test', group_id='yy_group', bootstrap_servers=bootstrap_servers)
-
-# Consume messages from the 'test_topic' topic
-for message in consumer:
-    print(f"Received message: {message.value}")
-
-# Close the consumer to release resources
-consumer.close()
+for msg in consumer:
+    print(msg)

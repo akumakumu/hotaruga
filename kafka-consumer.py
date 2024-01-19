1be407e5031893c466d61ee17cf1e42f110e2d2b
Yy <akumakumu@gmail.com>
2024-01-19 11:25:18 +0700

    only consuming one record

--- kafka-consumer.py
+++ kafka-consumer.py
@@ -1,21 +1,14 @@
 from kafka import KafkaConsumer
 
-#consumer = KafkaConsumer(bootstrap_servers='10.11.13.81:9092')
-
-#consumer.subscribe(['yytest'])
-
-#FORMAT
-#consumer = KafkaConsumer(
-#    subs_topic,
-#    bootstrap_servers='ip-address:port',
-#    auto_offset_reset='earliest' # consume from beginning topic
-#)
-
 consumer = KafkaConsumer(
-    'yytest',
     bootstrap_servers='10.11.13.81:9092',
+    group_id='yy',
     auto_offset_reset='earliest'
 )
 
-for msg in consumer:
-    print(msg)
+consumer.subscribe(['yytest'])
+
+message = next(consumer)
+print(message.value)
+
+consumer.close()

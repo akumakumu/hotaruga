e00451ecfa2ceb8ece3084cea2d128abdec8a21b
Yy <akumakumu@gmail.com>
2024-01-23 14:30:54 +0700

    consume kafka record to insert mongodb

--- kafka-consumer.py
+++ kafka-consumer.py
@@ -1,4 +1,19 @@
+from dotenv import load_dotenv
+from pymongo import MongoClient
 from kafka import KafkaConsumer
+
+import os
+import json
+
+load_dotenv()
+
+myURI = os.getenv('URI')
+myPORT = os.getenv('PORT')
+myDB = os.getenv('DB')
+
+client = MongoClient(myURI, int(myPORT))
+
+db = client[myDB]
 
 consumer = KafkaConsumer(
     bootstrap_servers='10.11.13.81:9092',
@@ -9,6 +24,9 @@ consumer = KafkaConsumer(
 consumer.subscribe(['jsontest'])
 
 message = next(consumer)
-print(message.value.decode('utf-8'))
+
+pointer = json.loads(message.value.decode('utf-8'))
+
+db.yytestdb.insert_one(pointer)
 
 consumer.close()
